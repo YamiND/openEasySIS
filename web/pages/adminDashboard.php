cc79e61557fcb681414d25ebd911b864d02b335a
Tyler Postma <comptvids@gmail.com>
2016-11-11 19:04:17 -0500

    Show total user count on admin panel

--- web/pages/adminDashboard.php
+++ web/pages/adminDashboard.php
@@ -21,6 +21,31 @@
             </div>
             <!-- /.row -->
             <div class="row">
+
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-users fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "adminProfile")) ?></div>
+                                    <div>Administrators</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
                 <div class="col-lg-3 col-md-6">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
@@ -67,6 +92,28 @@
                     </div>
                 </div>
 
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-users fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "schoolAdminProfile")) ?></div>
+                                    <div>School Administrators</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
                 <div class="col-lg-3 col-md-6">
                     <div class="panel panel-primary">
