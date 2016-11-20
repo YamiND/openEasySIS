e3f739a31f0412318671e1f26129e87c37c93b96
Tyler Postma <comptvids@gmail.com>
2016-11-19 21:30:34 -0500

    templated a bunch of the forms

--- web/pages/adminDashboard.php
+++ web/pages/adminDashboard.php
@@ -11,6 +11,9 @@
     <div id="wrapper">
 	<!-- Navigation Menu -->
         <?php include('../includes/navPanel.php'); ?>
+		<?php include('../includes/userFunctions/viewAnnouncements.php'); ?>
+		<?php include('../includes/adminFunctions/viewTotalAccounts.php'); ?>
+		<?php include('../includes/adminFunctions/viewTotalClasses.php'); ?>
 
         <div id="page-wrapper">
             <div class="row">
@@ -21,203 +24,30 @@
             </div>
             <!-- /.row -->
             <div class="row">
-<?php 
-if (roleID_check($mysqli) == 1):
-?>
+	<?php 
+		if (roleID_check($mysqli) == 1):
+	?>
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "adminProfile")); ?></div>
-                                    <div>Administrators</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="viewUserTables#Administrators">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+			<?php
+				viewAnnouncements($mysqli);
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "studentProfile")); ?></div>
-                                    <div>Students</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="viewUserTables#students">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+				viewTotalUsers($mysqli, "Administrators", "adminProfile");
+				viewTotalUsers($mysqli, "Students", "studentProfile");
+				viewTotalUsers($mysqli, "Teachers", "teacherProfile");
+				viewTotalUsers($mysqli, "Guardians", "guardianProfile");
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "teacherProfile")); ?></div>
-                                    <div>Teachers</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="viewUserTables#teachers">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+				viewTotalClasses($mysqli);
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "schoolAdminProfile")); ?></div>
-                                    <div>School Administrators</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="viewUserTables#schoolAdministrators">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?php echo(getTotalFromProfile($mysqli, "guardianProfile")); ?></div>
-                                    <div>Parents/Guardians</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="viewUserTables#guardians">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-green">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-university fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?php echo(getTotalClasses($mysqli)); ?></div>
-                                    <div>Classes</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-shopping-cart fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">124</div>
-                                    <div>New Orders!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-support fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Support Tickets!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-				<?php
-					viewAnnouncements($mysqli);
-				?>
+//				adminPasswordResetForm();
+			?>
             </div>
             <!-- /.row -->
 
-<?php 
-else:
-
-echo '<h1 class="text-danger">Invalid Page Access.</h1>';
-endif;
-?>
+	<?php 
+		else:
+			echo '<h1 class="text-danger">Invalid Page Access.</h1>';
+		endif;
+	?>
         </div>
         <!-- /#page-wrapper -->
     </div>
