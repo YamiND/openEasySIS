097a91d256ba273135f2c8bdf8112f0130ee7698
Tyler Postma <comptvids@gmail.com>
2016-11-19 21:30:16 -0500

    in process of fixing the password reset method

--- web/includes/passwordReset.php
+++ web/includes/passwordReset.php
@@ -4,26 +4,39 @@ include_once 'functions.php';
 
 sec_session_start(); // Our custom secure way of starting a PHP session.
 
-if (isset($_POST['email'], $_POST['oldPassword'], $_POST['newPassword'], $_POST['repeatPassword'])) 
+if (isset($_POST['userEmail'], $_POST['oldPassword'], $_POST['newPassword'], $_POST['repeatPassword'])) 
 {
-    $email = $_POST['email'];
+    $userEmail = $_POST['userEmail'];
     
     $oldPassword = $_POST['oldPassword']; // The hashed old password.
     $newPassword = $_POST['newPassword']; // The hashed new password.
     $repeatPassword = $_POST['repeatPassword']; // The hashed new repeattd password.
 
-   echo $email . "\n";
-   echo $oldPassword . "\n";
-   echo $newPassword . "\n";
-   echo $repeatPassword . "\n";
-    if (login($email, $oldPassword, $mysqli) == true)
+    if (login($userEmail, $oldPassword, $mysqli) == true)
     {
         // Do a second check to make sure the password is right
-	if($newPassword == $repeatPassword)
-	{
-//	    changePasword($email, $oldPassword, $newPassword, $mysqli);
-//            header('Location: ../pages/settings.php?success=1');
-	}	
+		if($newPassword == $repeatPassword)
+		{
+
+			if ($stmt = $mysqli->prepare("SELECT userSalt FROM users WHERE userEmail = ? LIMIT 1"))
+			{
+				$stmt->bind_param('s', $userEmail);
+				$stmt->execute();
+				$stmt->store_result();
+
+				$stmt->bind_result($userSalt);
+				$stmt->fetch();
+
+				
+				$oldPassword = hash('sha512', $oldPassword . $userSalt);	
+
+				// I don't see an issue with using the same salt
+				$newPassword = hash('sha512', $newPassword . $userSalt);
+
+				echo "Before changePassword function <br>";
+				changePassword($userEmail, $oldPassword, $newPassword, $mysqli);
+			}
+		}	
     }
     else
     {
