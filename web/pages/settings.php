c364e0dcd8b35cc97f11b041a05a4fefd6ab797b
Tyler Postma <comptvids@gmail.com>
2016-11-19 23:15:39 -0500

    Cleaned up settings and login

--- web/pages/settings.php
+++ web/pages/settings.php
@@ -2,29 +2,29 @@
 <html lang="en">
 
 <head>
-    <title>openEasySIS - Overview</title>
+    <title>openEasySIS - Settings</title>
     <!-- Header Information, CSS, and JS -->
     <?php include("../includes/header.php"); ?>
-    <script type="text/JavaScript" src="../js/sha512.js"></script> 
-    <script type="text/JavaScript" src="../js/passwordReset.js"></script> 
+   <!-- <script type="text/JavaScript" src="../js/sha512.js"></script> 
+    <script type="text/JavaScript" src="../js/passwordReset.js"></script> -->
 </head>
 
 <body>
     <div id="wrapper">
 	<!-- Navigation Menu -->
         <?php include('../includes/navPanel.php'); ?>
-
+		<?php include('../includes/userFunctions/viewUserPasswordResetForm.php'); ?>
         <div id="page-wrapper">
        	    <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Forms</h1>
+                    <h1 class="page-header">Settings</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
 
 
 	<?php
-		userPasswordResetForm();
+		viewUserPasswordResetForm($mysqli);
 	?>
 
         </div>
