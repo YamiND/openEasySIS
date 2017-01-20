9409af62e23e35b6bcb30a18f1d4b8f788b897e8
Tyler Postma <comptvids@gmail.com>
2017-01-20 16:18:02 -0500

    forgot page

---
+++ web/pages/viewGradeForStudent.php
@@ -0,0 +1,66 @@
+<?php include("../includes/customizations.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title><?php echo aliasOpenEasySIS; ?> - Student Grade</title>
+    <!-- Header Information, CSS, and JS -->
+    <?php include("../includes/header.php"); ?>
+
+    <!-- DataTables CSS -->
+    <link href="../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+	<!-- DataTables JavaScript -->
+    <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+    <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+	<!-- Navigation Menu -->
+        <?php include('../includes/navPanel.php'); ?>
+		<?php include('../includes/teacherFunctions/viewGradeForStudentTable.php'); ?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Student Grades</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+
+			<?php
+				viewGradesForStudentTable($mysqli)
+			?>
+            <!-- /.row -->
+                                </tbody>
+                            </table>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+			<!-- End Teachers -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+</body>
+
+</html>
