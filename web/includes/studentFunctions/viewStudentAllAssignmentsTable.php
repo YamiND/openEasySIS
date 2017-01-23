63fb29a50593ae469bc538c0494f568822d728c7
Tyler Postma <comptvids@gmail.com>
2017-01-23 15:20:56 -0500

    Added ability for students to view all assignments and their grades

--- web/includes/studentFunctions/viewStudentAllAssignmentsTable.php
+++ web/includes/studentFunctions/viewStudentAllAssignmentsTable.php
@@ -1,10 +1,11 @@
 <?php
+
 //TODO: Test this after adding multiple students to a class
 function checkPermissions($mysqli)
 {
     if ((login_check($mysqli) == true) && (roleID_check($mysqli) == 5))
     {
-        viewStudentAllAssignmentsTable($mysqli);
+        viewGradeForStudentTable($mysqli);
     }
     else
     {
@@ -14,7 +15,7 @@ function checkPermissions($mysqli)
     }
 }
 
-function viewStudentAllAssignmentsTable($mysqli)
+function viewGradeForStudentTable($mysqli)
 {
 	echo '
             <div class="row">
@@ -23,171 +24,138 @@ function viewStudentAllAssignmentsTable($mysqli)
                         <div class="panel-heading">
 	     ';
 						// Call Session Message code and Panel Heading here
-                        displayPanelHeading("All Assignments");
-    echo '
+                        displayPanelHeading("View Grade for Student");
+echo '
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
+                            <!-- Nav tabs -->
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a href="#addMaterialType" data-toggle="tab">Student Grades</a>
+                                </li>
+                            </ul>
+
+                            <!-- Tab panes -->
+                            <div class="tab-content">
+                                <div class="tab-pane fade in active" id="selectAssignment">';
 
                             
-        ';
-                                viewAllAssignments($mysqli);
-    echo '
-                                
+                                    viewStudentAssignments($_SESSION['userID'], $mysqli);
+echo '
+                                </div>
+                ';
+echo '
+                            </div>
                         </div>
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
                 </div>
 			</div>
-        ';
+';
 
 }
 
-function viewAllAssignments($mysqli)
+function viewStudentAssignments($studentID, $mysqli)
 {
-    if ($stmt = $mysqli->prepare("SELECT classID FROM studentClassIDs WHERE studentID = ?"))
-    {
-        $stmt->bind_param('i', $_SESSION['userID']);
+	$yearID = getClassYearID($mysqli);
+
+	if ($stmt = $mysqli->prepare("SELECT studentClassIDs.classID, className FROM studentClassIDs, classes WHERE studentID = ? AND schoolYearID = ?"))
+	{
+    	$stmt->bind_param('ii', $studentID, $yearID);
         $stmt->execute();
-        $stmt->bind_result($classID);
+        $stmt->bind_result($classID, $className);
         $stmt->store_result();
 
-        while($stmt->fetch())
-        {   
-            echo '
-                    <h4> Class Name: ' . getClassName($classID, $mysqli) . '</h4>
-                    <br>
-                    <table width="100%" class="table table-striped table-bordered table-hover" id="' . $classID . '">
-                        <thead>
-                            <tr>
-                                <th>Assignment Name</th>
-                                <th>Assignment Type</th>
-                                <th>Assignment Due Date</th>
-                                <th>Assignment Points Scored</th>
-                                <th>Assignment Points Possible</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                ';          
-                            getAllAssignmentsForClass($classID, $_SESSION['userID'], $mysqli);
-            echo ' 
-                        </tbody>
-                    </table>
-                    <!-- /.table-responsive -->
 
-                    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-                    <script>
-                    $(document).ready(function() {
-                        $(\'#' . $classID . '\').DataTable({
-                            responsive: true
-                        });
+		while ($stmt->fetch())
+		{
+			echo '
+				<h4> ' . $className . ' </h4>
+                                <table width="100%" class="table table-striped table-bordered table-hover" id="' . $studentID . '">
+                                    <thead>
+                                        <tr>
+                                            <th>Assignment Name</th>
+                                            <th>Assignment Due Date</th>
+                                            <th>Assigment Type</th>
+                                            <th>Points Scored</th>
+                                            <th>Points Possible</th>
+                                            <th>Assignment Grade</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+	        ';          
+					getAssignmentsForStudentClass($studentID, $classID, $mysqli);
+		    echo ' 
+                                    </tbody>
+                                </table>
+                                <!-- /.table-responsive -->
+
+                <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+                <script>
+                $(document).ready(function() {
+                    $(\'#' . $studentID . '\').DataTable({
+                        responsive: true
                     });
-                    </script>
-            ';
-        }           
-    }
-    else
-    {
-        echo "No Clases";
-        return;
-    }
+                });
+                </script>
+       		 ';
+		}
+	}
 }
 
-function getAllAssignmentsForClass($classID, $studentID, $mysqli)
+function getAssignmentsForStudentClass($studentID, $classID, $mysqli)
 {
-    if ($stmt = $mysqli->prepare("SELECT materialID, materialName, materialPointsPossible, materialTypeID, materialDueDate FROM materials WHERE materialClassID = ?"))
+    if ($stmt = $mysqli->prepare("SELECT materialID, materialName, materialPointsPossible, materialDueDate, materialTypeID FROM materials WHERE materialClassID = ?"))
     {
         $stmt->bind_param('i', $classID);
         $stmt->execute();
-        $stmt->bind_result($materialID, $materialName, $materialPointsPossible, $materialTypeID, $materialDueDate);
+        $stmt->bind_result($materialID, $materialName, $materialPointsPossible, $materialDueDate, $materialTypeID);
         $stmt->store_result();
 
-        while($stmt->fetch())
-        {
-                
-            echo '
+		while ($stmt->fetch())
+		{
+			$materialPointsScored = getMaterialPointsScored($materialID, $classID, $studentID, $mysqli);
+
+			$materialTotalPoints = ($materialPointsScored / $materialPointsPossible) * 100;
+
+			 echo '
                     <tr class="gradeA">
-                        <td>' . $materialName . '</td>
-                        <td>' . getMaterialType($materialTypeID, $mysqli) . '</td>
-                        <td> '. $materialDueDate . '</td>
-                        <td>' . getMaterialPointsScored($materialID, $studentID, $classID, $mysqli) . '</td>
+                        <td>' . $materialName. '</td>
+                        <td>' . $materialDueDate . '</td>
+                        <td>' . getMaterialTypeName($materialTypeID, $mysqli) . '</td>
+                        <td>' . $materialPointsScored . '</td>
                         <td> /' . $materialPointsPossible . '</td>
+                        <td>' . $materialTotalPoints . '%</td>
                     </tr>
-                ';     
-        }           
-    }
-    else
-    {
-        return "0";
+                ';
+		
+		}
     }
 }
 
-function getMaterialPointsScored($materialID, $studentID, $classID, $mysqli)
-{
-    if ($stmt = $mysqli->prepare("SELECT gradeMaterialPointsScored FROM grades WHERE gradeMaterialID = ? AND gradeClassID = ? AND gradeStudentID = ?"))
-    {
-        $stmt->bind_param('iii', $materialID, $classID, $studentID);
-        $stmt->execute();
-        $stmt->bind_result($materialPointsScored);
-        $stmt->store_result();
-
-        if ($stmt->num_rows > 0)
-        {
-            while($stmt->fetch())
-            {
-                return $materialPointsScored;     
-            }                       
-        }
-        else
-        {
-            return "Assignment not yet Graded";
-        }
-    }
-    else
-    {
-        return "Assignment not yet Graded";
-    }
-}
-
-function getMaterialType($materialTypeID, $mysqli)
+function getMaterialTypeName($materialTypeID, $mysqli)
 {
     if ($stmt = $mysqli->prepare("SELECT materialName FROM materialType WHERE materialTypeID = ?"))
-    {
+    {   
         $stmt->bind_param('i', $materialTypeID);
         $stmt->execute();
-        $stmt->bind_result($materialName);
+        $stmt->bind_result($materialTypeNameResult);
         $stmt->store_result();
 
-        while($stmt->fetch())
-        {
-            return $materialName;     
-        }           
-    }
+    if ($stmt->num_rows > 0)
+    {   
+        while ($stmt->fetch())
+        {   
+            return $materialTypeNameResult;
+        }   
+    }   
     else
-    {
-        return "N/A";
-    }
-}
+    {   
+        return "No Assignment Types";
+    }   
 
-function getClassName($classID, $mysqli)
-{
-    if ($stmt = $mysqli->prepare("SELECT className FROM classes WHERE classID = ?"))
-    {
-        $stmt->bind_param('i', $classID);
-        $stmt->execute();
-        $stmt->bind_result($className);
-        $stmt->store_result();
-
-        while($stmt->fetch())
-        {
-            return $className;     
-        }           
-    }
-    else
-    {
-        return "0";
-    }
+    }   
 }
 
 ?>
