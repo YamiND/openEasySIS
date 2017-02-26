6c4fd34fca6ff6b14d38ace7a2e9a44f1a1aee73
Tyler Postma <comptvids@gmail.com>
2017-02-25 19:48:52 -0500

    Reduced profile functionality, but fixed code

--- web/includes/userFunctions/viewProfile.php
+++ web/includes/userFunctions/viewProfile.php
@@ -37,7 +37,7 @@ function viewProfile($mysqli)
                                 <div class="tab-pane fade in active" id="myProfile">
                                 <br>
         ';
-                                getUserInfoForm($_SESSION['userID'], $mysqli);
+								getUserProfile($_SESSION['userID'], $mysqli);
     echo '
                                     
                                 </div>
@@ -52,39 +52,9 @@ function viewProfile($mysqli)
 
 }
 
-function getUserInfoForm($userID, $mysqli)
-{
-	// TODO: Adapt this for multiple profile showings....
-
-	if (isAdmin($mysqli))
-	{
-		getUserProfile($userID, $mysqli);
-	}
-
-	if (isSchoolAdmin($mysqli))
-	{
-		getUserProfile($userID, $mysqli);
-	}
-	
-	if (isTeacher($mysqli))
-	{
-		getUserProfile($userID, $mysqli);
-	}
-
-	if (isParent($mysqli))
-	{
-    	getParentProfile($userID, $mysqli);
-	}
-
-	if (isStudent($mysqli))
-	{
-    	getStudentProfile($userID, $mysqli);
-	}
-}
-
 function getUserProfile($userID, $mysqli)
 {
-    if ($stmt = $mysqli->prepare("SELECT userFirstName, userLastName, userEmail FROM users WHERE userID = ?"))
+    if ($stmt = $mysqli->prepare("SELECT userFirstName, userLastName, userEmail  FROM users WHERE userID = ?"))
     {   
         // Query database for announcement details to be modified
         $stmt->bind_param('i', $userID);
@@ -102,104 +72,6 @@ function getUserProfile($userID, $mysqli)
                 generateFormInputDiv("Last Name", "text", "userLastName", $userLastName, "disabled");
                 generateFormInputDiv("Email", "email", "userEmail", $userEmail, "disabled");
             generateFormEnd();
-        }
-    }
-}
-
-function getParentProfile($userID, $mysqli)
-{
-    if ($stmt = $mysqli->prepare("SELECT parentFirstName, parentLastName, parentEmail, parentPhoneNumber, parentAltEmail, parentAddress, parentCity, parentState, parentZip FROM parentProfile WHERE parentID = ?"))
-    {   
-        // Query database for announcement details to be modified
-        $stmt->bind_param('i', $userID);
-
-        $stmt->execute();
-
-        $stmt->bind_result($parentFirstName, $parentLastName, $parentEmail, $parentPhoneNumber, $parentAltEmail, $parentAddress, $parentCity, $parentState, $parentZip);
-        
-        $stmt->store_result();
-        
-        while ($stmt->fetch())
-        {
-                generateFormStart();
-
-                    generateFormInputDiv("First Name", "text", "parentFirstName", $parentFirstName, "disabled");
-                    generateFormInputDiv("Last Name", "text", "parentLastName", $parentLastName, "disabled");
-                    generateFormInputDiv("Email", "email", "parentEmail", $parentEmail, "disabled");
-
-                    if (!empty($parentPhoneNumber))
-                    {
-                        generateFormInputDiv("Phone Number", "tel", "parentPhoneNumber", $parentPhoneNumber, "disabled");
-                    }
-
-                    if (!empty($parentAltEmail))
-                    {
-                        generateFormInputDiv("Alt Email", "email", "parentAltEmail", $parentAltEmail, "disabled");
-                    }
-
-                    generateFormInputDiv("Address", "text", "parentAddress", $parentAddress, "disabled");
-                    generateFormInputDiv("City", "text", "parentCity", $parentCity, "disabled");
-                    generateFormInputDiv("State", "text", "parentState", $parentState, "disabled");
-                    generateFormInputDiv("Zip", "number", "parentZip", $parentZip, "disabled");
-
-
-                    generateFormStartSelectDiv("Students", "students");
-                        getStudentList($userID, $mysqli);
-                    generateFormEndSelectDiv();
-
-                generateFormEnd();
-        }
-    }
-}
-
-function getStudentProfile($userID, $mysqli)
-{
-    if ($stmt = $mysqli->prepare("SELECT studentFirstName, studentLastName, studentEmail, studentBirthdate, studentGender, studentGradYear, studentGPA, studentGradeLevel FROM studentProfile WHERE studentID = ?"))
-    {   
-        // Query database for announcement details to be modified
-        $stmt->bind_param('i', $userID);
-
-        $stmt->execute();
-
-        $stmt->bind_result($studentFirstName, $studentLastName, $studentEmail, $studentBirthdate, $studentGender, $studentGradYear, $studentGPA, $studentGradeLevel);
-        
-        $stmt->store_result();
-        
-        while ($stmt->fetch())
-        {
-
-            generateFormStart();
-
-                generateFormInputDiv("First Name", "text", "studentFirstName", $studentFirstName, "disabled");
-                generateFormInputDiv("Last Name", "text", "studentLastName", $studentLastName, "disabled");
-                generateFormInputDiv("Email", "email", "studentEmail", $studentEmail, "disabled");
-                generateFormInputDiv("Birthdate", "text", "studentBirthdate", $studentBirthdate, "disabled");
-                generateFormInputDiv("Gender", "text", "studentGender", $studentGender, "disabled");
-                generateFormInputDiv("Graduation Year", "number", "studentGradYear", $studentGradYear, "disabled");
-                generateFormInputDiv("GPA", "number", "studentGPA", $studentGPA, "disabled");
-                generateFormInputDiv("Grade Level", "number", "studentGradeLevel", $studentGradeLevel, "disabled");
-
-            generateFormEnd();
-        }
-    }
-}
-
-function getStudentList($userID, $mysqli)
-{
-    if ($stmt = $mysqli->prepare("SELECT studentID FROM studentParentIDs WHERE parentID = ?"))
-    {   
-        // Query database for announcement details to be modified
-        $stmt->bind_param('i', $userID);
-
-        $stmt->execute();
-
-        $stmt->bind_result($studentID);
-        
-        $stmt->store_result();
-        
-        while ($stmt->fetch())
-        {
-            getUserName($studentID, $mysqli);
         }
     }
 }
