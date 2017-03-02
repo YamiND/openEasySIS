8e63f85b2cb78bc420eefb1fa7d928d5bfface66
Tyler Postma <comptvids@gmail.com>
2017-03-01 22:54:20 -0500

    Revamped page for new bulk upload of users

--- web/includes/adminFunctions/viewCreateBulkUserForm.php
+++ web/includes/adminFunctions/viewCreateBulkUserForm.php
@@ -4,7 +4,7 @@ function checkPermissions($mysqli)
 {
     if (login_check($mysqli) == true)
     {
-        viewCreateBulkUserForm($mysqli);
+		viewCreateBulkUserForm($mysqli);
     }
     else
     {
@@ -16,31 +16,58 @@ function checkPermissions($mysqli)
 
 function viewCreateBulkUserForm($mysqli)
 {
+
 	echo '
-            <div class="row">
-                <div class="col-lg-6">
+			<div class="col-lg-6">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-	   ';
+	';
                         // Call Session Message code and Panel Heading here
-						displayPanelHeading("Create User (CSV)");
-    echo '
+						displayPanelHeading("Create Users (CSV)");
+	echo '
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <!-- Nav tabs -->
                             <ul class="nav nav-tabs">
-                                <li class="active"><a href="#createUser" data-toggle="tab">Create Users in Bulk</a>
+                                <li class="active"><a href="#isStudent" data-toggle="tab">Students</a>
+                                </li>
+                                <li><a href="#isParent" data-toggle="tab">Parents</a>
+                                </li>
+                                <li><a href="#isTeacher" data-toggle="tab">Teachers</a>
+                                </li>
+                                <li><a href="#isSchoolAdmin" data-toggle="tab">School Admins</a>
+                                </li>
+                                <li><a href="#isAdmin" data-toggle="tab">Admins</a>
                                 </li>
                             </ul>
 
                             <!-- Tab panes -->
                             <div class="tab-content">
-                                <div class="tab-pane fade in active" id="createUser">
-        ';
-	    						getUploadForm();      
-    echo '
-                                    
+                                <div class="tab-pane fade in active" id="isStudent">
+								';
+									echo getStudentUploadForm();
+						echo '
+                                </div>
+                                <div class="tab-pane fade" id="isParent">
+							';
+									echo getParentUploadForm();
+						echo '
+                                </div>
+                                <div class="tab-pane fade" id="isTeacher">
+							';
+									echo getTeacherUploadForm();
+						echo '
+                                </div>
+                                <div class="tab-pane fade" id="isSchoolAdmin">
+							';
+									echo getSchoolAdminUploadForm();
+						echo '
+                                </div>
+                                <div class="tab-pane fade" id="isAdmin">
+							';
+									echo getAdminUploadForm();
+						echo '
                                 </div>
                             </div>
                         </div>
@@ -48,19 +75,61 @@ function viewCreateBulkUserForm($mysqli)
                     </div>
                     <!-- /.panel -->
                 </div>
-			</div>
-        ';
-
+';
 }
 
-function getUploadForm()
+function getStudentUploadForm()
 {
     generateFormStart("../includes/adminFunctions/createBulkUser", "post", "multipart/form-data"); 
-	echo '<h4>Upload User CSV</h4>';
+	echo '<h4>Upload Student CSV</h4>';
+	echo '<input type="hidden" name="isStudent" value="1" />';
 	echo '<input type="file" name="csvFile" id="file" />';
 	echo "<br>";
-        generateFormButton("uploadCSV", "Upload CSV and Create Users");
+        generateFormButton("uploadCSV", "Upload CSV and Create Students");
     generateFormEnd();
 }
 
+function getParentUploadForm()
+{
+    generateFormStart("../includes/adminFunctions/createBulkUser", "post", "multipart/form-data"); 
+	echo '<h4>Upload Parent CSV</h4>';
+	echo '<input type="hidden" name="isParent" value="1" />';
+	echo '<input type="file" name="csvFile" id="file" />';
+	echo "<br>";
+        generateFormButton("uploadCSV", "Upload CSV and Create Parents");
+    generateFormEnd();
+}
+
+function getTeacherUploadForm()
+{
+    generateFormStart("../includes/adminFunctions/createBulkUser", "post", "multipart/form-data"); 
+	echo '<h4>Upload Teacher CSV</h4>';
+	echo '<input type="hidden" name="isTeacher" value="1" />';
+	echo '<input type="file" name="csvFile" id="file" />';
+	echo "<br>";
+        generateFormButton("uploadCSV", "Upload CSV and Create Teachers");
+    generateFormEnd();
+}
+
+function getSchoolAdminUploadForm()
+{
+    generateFormStart("../includes/adminFunctions/createBulkUser", "post", "multipart/form-data"); 
+	echo '<h4>Upload School Admin CSV</h4>';
+	echo '<input type="hidden" name="isSchoolAdmin" value="1" />';
+	echo '<input type="file" name="csvFile" id="file" />';
+	echo "<br>";
+        generateFormButton("uploadCSV", "Upload CSV and Create School Administrators");
+    generateFormEnd();
+}
+
+function getAdminUploadForm()
+{
+    generateFormStart("../includes/adminFunctions/createBulkUser", "post", "multipart/form-data"); 
+	echo '<h4>Upload Admin CSV</h4>';
+	echo '<input type="hidden" name="isAdmin" value="1" />';
+	echo '<input type="file" name="csvFile" id="file" />';
+	echo "<br>";
+        generateFormButton("uploadCSV", "Upload CSV and Create School Administrators");
+    generateFormEnd();
+}
 ?>
