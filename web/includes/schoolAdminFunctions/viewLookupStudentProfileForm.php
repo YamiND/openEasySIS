efe95c5bd72e92462ad7f5acd096c56ccbf0360d
Tyler Postma <comptvids@gmail.com>
2017-03-25 22:31:55 -0400

    Added ability for special permission access

--- web/includes/schoolAdminFunctions/viewLookupStudentProfileForm.php
+++ web/includes/schoolAdminFunctions/viewLookupStudentProfileForm.php
@@ -12,7 +12,7 @@ if (isset($_POST['changeUser']))
 
 function checkPermissions($mysqli)
 {
-    if ((login_check($mysqli) == true) && (isSchoolAdmin($mysqli)))
+    if ((login_check($mysqli) == true) && (isSchoolAdmin($mysqli) || canModClassList($mysqli)))
     {
         viewEditProfileForm($mysqli);
     }
